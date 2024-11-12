b8f9d15598b2e4e6c7fa6a36fa9c9b079eb28640
Ahmad Akbar Mubarak <130969627+ahmakbardev@users.noreply.github.com>
2024-11-13 05:47:47 +0700

    try close modal

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -33,9 +33,9 @@
 
 
     <!-- Append version number to CSS file name -->
-    <link rel="stylesheet" href="{{ asset('css/app.css?v=1.01') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css?v=1.02') }}">
     <!-- Add cache-control headers for CSS and JavaScript files -->
-    <link rel="preload" href="{{ asset('css/app.css?v=1.01') }}" as="style" crossorigin="anonymous" />
+    <link rel="preload" href="{{ asset('css/app.css?v=1.02') }}" as="style" crossorigin="anonymous" />
 
     <link href="{{ asset('css/swiper.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/swiper.css') }}" rel="stylesheet" />
